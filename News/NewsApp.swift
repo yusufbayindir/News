ffbc564758d9e6ff0c968f54104f1fd21f9ea0be
yusufbayindir <bayindiryusuf0@gmail.com>
2024-02-24 17:00:48 +0300

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Yusuf Bayindir on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
